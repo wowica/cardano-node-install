ae9bc07a9d33836af8b70add5934cbabed29062c
Carlos Souza <caike@users.noreply.github.com>
2023-06-07 12:49:16 -0400

    Update 09-query_tip.sh
    Set socket path from current directory

--- scripts/09-query_tip.sh
+++ scripts/09-query_tip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CARDANO_NODE_SOCKET_PATH=/home/ubuntu/db/node.socket cardano-cli query tip --testnet-magic 2
+CARDANO_NODE_SOCKET_PATH="$PWD/db/node.socket" cardano-cli query tip --testnet-magic 2
