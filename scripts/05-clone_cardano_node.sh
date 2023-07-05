87735ed12706f075f023a11701174f8c27403759
Carlos Souza <caike@users.noreply.github.com>
2023-07-05 09:51:31 -0400

    Clone node 8.1.1

--- scripts/05-clone_cardano_node.sh
+++ scripts/05-clone_cardano_node.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/input-output-hk/cardano-node.git
 cd cardano-node
 git fetch --all --recurse-submodules --tags
-git checkout $(curl -s https://api.github.com/repos/input-output-hk/cardano-node/releases/latest | jq -r .tag_name)
+git checkout 8.1.1
 
 echo
 echo "Finished cloning cardano-node"
